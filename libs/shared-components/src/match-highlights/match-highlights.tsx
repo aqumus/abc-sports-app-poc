25c8e16274f4db3a10d875a9c66d3b5c01f0188d
Aquib Vadsaria <vadsaria.ak@gmail.com>
2022-09-26 18:55:24 +0530

    fix: match highlights width card

--- libs/shared-components/src/match-highlights/match-highlights.tsx
+++ libs/shared-components/src/match-highlights/match-highlights.tsx
@@ -23,7 +23,7 @@ function MatchHighlightComponent({
   }, [videoDetails, displayed]);
 
   return (
-    <div className="bg-slate-100 dark:bg-slate-800 flex flex-col p-6 min-w-40 justify-between relative">
+    <div className="bg-slate-100 dark:bg-slate-800 flex flex-col p-6 min-w-fit justify-between relative">
       {displayed ? (
         <div>
           <button
@@ -44,11 +44,11 @@ function MatchHighlightComponent({
       ) : (
         <>
           <h2 className="text-xl">{title}</h2>
-          <span className="text-slate-500 dark:text-slate-400 text-sm mt-4">
+          <span className="text-slate-500 dark:text-slate-400 text-sm mt-4 w-60">
             {description}
           </span>
           <button
-            className="bg-slate-200 dark:bg-slate-700 hover:bg-slate-300 hover:dark:bg-slate-600 m-4 p-2"
+            className="bg-slate-200 dark:bg-slate-700 hover:bg-slate-300 hover:dark:bg-slate-600 m-auto mt-8 p-2 w-20"
             onClick={toggleVideo}
           >
             Watch
